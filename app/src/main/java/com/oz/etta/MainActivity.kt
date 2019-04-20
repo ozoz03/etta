9e54c4b35806da6d86eeae0209828a3507f2a3ab
ozasymenko <oleg.zasymenko@vgs.io>
2019-04-20 10:25:37 +0300

    Fixed STOP/START, gitignore, added result persist

--- app/src/main/java/com/oz/etta/MainActivity.kt
+++ app/src/main/java/com/oz/etta/MainActivity.kt
@@ -4,19 +4,24 @@ import android.app.ActionBar
 import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.*
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.io.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    var gameIsStarted = false
+    val ACTIVE_BUTTON_COLOR = Color.GREEN
+    val INACTIVE_BUTTON_COLOR = Color.GRAY
+
+    var isGameStarted = false
     var buttonList = LinkedHashSet<UserButton>()
     var currentUserButton: UserButton? = null
     var userIterator: Iterator<UserButton>? = null
+    val scoreFileName = "scoreMap.txt"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,16 +32,10 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-
-//        viewUserScore.text = ""
-//        editUserScore.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
-//            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-//                buttonAddToScore.callOnClick()
-//                return@OnKeyListener true
-//            }
-//            false
-//        })
-
+        val file = File(applicationContext.filesDir, scoreFileName)
+        if (file.exists()) {
+            mapToUsers(readFromFile(scoreFileName))
+        }
 
         buttonAddToScore.setOnClickListener {
             if (editUserScore.text.trim().isBlank() || !editUserScore.text.toString().get(0).isDigit()) {
@@ -49,6 +48,7 @@ class MainActivity : AppCompatActivity() {
             currentUserButton!!.updateText()
 
             getNextUser()
+            writeToFile(scoreFileName, usersToMap())
         }
 
         buttonAddUser.setOnClickListener {
@@ -56,6 +56,9 @@ class MainActivity : AppCompatActivity() {
             val newButton = UserButton(this, newUser)
             buttonList.add(newButton)
             newButton.updateText()
+            newButton.setOnClickListener {
+                makeMyUserActive(newButton)
+            }
 
             val lp = ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
             userGridLayout.addView(newButton, lp)
@@ -63,26 +66,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         start_stop_button.setOnClickListener {
-            if (gameIsStarted) {
+            if (isGameStarted) {
                 start_stop_button.text = "STOP GAME"
-                gameIsStarted = false
-//                editUserScore.setBackgroundColor(Color.WHITE)
-                changeLayoutState(addNewUserLayout, gameIsStarted)
-                changeLayoutState(addScoreLayout, !gameIsStarted)
+                isGameStarted = false
+                changeLayoutState(addNewUserLayout, enabled = true)
+                changeLayoutState(addScoreLayout, enabled = false)
             } else {
                 if (!buttonList.isEmpty()) {
                     start_stop_button.text = "START GAME"
-                    gameIsStarted = true
-                    changeLayoutState(addNewUserLayout, gameIsStarted)
-                    changeLayoutState(addScoreLayout, !gameIsStarted)
+                    isGameStarted = true
+                    changeLayoutState(addNewUserLayout, enabled = false)
+                    changeLayoutState(addScoreLayout, enabled = true)
                     userIterator = buttonList.iterator()
                     getNextUser()
 
                     // set focus to edit score
                     editUserScore.requestFocus()
-//                    editUserScore.setBackgroundColor(Color.RED)
-//                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//                    imm.showSoftInput(editUserScore, InputMethodManager.SHOW_IMPLICIT)
                 }
             }
         }
@@ -91,29 +90,77 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun mapToUsers(userScoreMap: Map<String, Integer>) {
+        for (entry in userScoreMap) {
+            val newUser = User(entry.key, entry.value)
+            val newButton = UserButton(this, newUser)
+            buttonList.add(newButton)
+            val lp = ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
+            userGridLayout.addView(newButton, lp)
+        }
+    }
+
+    private fun makeMyUserActive(thisButton: UserButton) {
+        if (isGameStarted) {
+            currentUserButton!!.setBackgroundColor(INACTIVE_BUTTON_COLOR)
+            currentUserButton = thisButton
+            currentUserButton!!.setBackgroundColor(ACTIVE_BUTTON_COLOR)
+            updateEditForm()
+            // reset iterator
+            userIterator = buttonList.iterator()
+        }
+    }
+
+    private fun usersToMap(): Map<String, Integer> {
+        val userMap = HashMap<String, Integer>()
+        for (userButton in buttonList) {
+            userMap.put(userButton.user.name, userButton.user.score)
+        }
+        return userMap
+    }
+
+    private fun writeToFile(fileName: String, userScoreMap: Map<String, Integer>) {
+        val file = File(applicationContext.filesDir, fileName)
+        val fileOutputStream = FileOutputStream(file)
+        val objectOutputStream = ObjectOutputStream(fileOutputStream)
+
+        objectOutputStream.writeObject(userScoreMap)
+        objectOutputStream.close()
+    }
+
+    private fun readFromFile(fileName: String): Map<String, Integer> {
+        val file = File(applicationContext.filesDir, fileName)
+
+        val fileInputStream = FileInputStream(file)
+        val objectInputStream = ObjectInputStream(fileInputStream)
+
+        val myNewlyReadInMap = objectInputStream.readObject() as Map<String, Integer>
+        objectInputStream.close()
+        return myNewlyReadInMap
+    }
+
     private fun getNextUser() {
         if (currentUserButton != null) {
-            currentUserButton!!.setBackgroundColor(Color.GRAY)
+            currentUserButton!!.setBackgroundColor(INACTIVE_BUTTON_COLOR)
         }
         if (!userIterator!!.hasNext()) {
             userIterator = buttonList.iterator()
         }
         currentUserButton = userIterator!!.next()
 
-        currentUserButton!!.setBackgroundColor(Color.GREEN)
+        currentUserButton!!.setBackgroundColor(ACTIVE_BUTTON_COLOR)
 
         updateEditForm()
     }
 
     private fun updateEditForm() {
         textViewUserName.setText(currentUserButton!!.user.toString())
-//        viewUserScore.setText(currentUserButton!!.user.score.toString())
     }
 
-    private fun changeLayoutState(layout: ViewGroup, state: Boolean) {
+    private fun changeLayoutState(layout: ViewGroup, enabled: Boolean) {
         for (i in 0 until layout.childCount) {
             val child = layout.getChildAt(i)
-            child.isEnabled = state
+            child.isEnabled = enabled
         }
     }
 
